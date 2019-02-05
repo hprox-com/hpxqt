a33facc07ef584efe85fd1c62a57ee4950e17006
kuchtak <k.kuchta@bioinfo.pl>
2019-02-05 14:49:55 +0100

    Fixing resource path for py2app.

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ def get_data_dir():
 
         # py2app binary
         return os.path.realpath(os.path.dirname(
-            os.path.dirname(os.path.dirname(__file__)))
+            os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
         )
 
     return os.path.dirname(os.path.abspath(__file__))
