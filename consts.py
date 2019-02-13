91655d6fd2570f6026eee68886088112a84f5b5c
Damian Peszko <d.peszko@bioinfo.pl>
2019-02-13 13:12:25 +0100

    Defined consts

---
+++ consts.py
@@ -0,0 +1,25 @@
+APP_NAME = 'hprox'
+APP_TITLE = 'hprox.com'
+
+URL_PREFIX = 'https://hprox.com/'
+
+START_DOWNLOAD = 1
+FINISHED_DOWNLOAD = 2
+START_INSTALL = 3
+FINISHED_INSTALL = 4
+
+LINUX_APP_NAME = 'hprox'
+MAC_APP_NAME = 'hprox.app'
+WINDOWS_APP_NAME = 'hprox.exe'
+
+ARCH_MAP = {
+    '64bit': '64',
+    '32bit': '32'
+}
+
+MAC_OS = 'osx'
+LINUX_OS = 'linux'
+WINDOWS_OS = 'windows'
+
+COMPRESSED_FILE_OS = [MAC_OS, LINUX_OS]
+
